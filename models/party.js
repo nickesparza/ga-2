8dcb69137327d11f7349b329588f7d712e6a16b8
nickesparza <nickesparza@gmail.com>
2022-07-12 12:50:02 -0500

    initial setup of party and user models

---
+++ models/party.js
@@ -0,0 +1,27 @@
+// require existing mongoose connection to db
+const mongoose = require('./connection')
+// will require snack schema for subdocuments later on
+// TODO: create Snack schema
+// const snackSchema = require('./snack')
+
+const { Schema, model } = mongoose
+
+const partySchema = new Schema({
+    name: { type: String, required: true },
+    date: String,
+    // this will refer to the owner of the party for the purposes of displaying a user's parties
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    watched: { type: Boolean, default: false },
+    // I might not need this attribute
+    archived: Boolean,
+    // movies will be a schema ref
+    // TODO: create and link movie schema
+    // movies: [movieSchema]???
+})
+
+const Party = model('Party', partySchema)
+
+module.exports = Party
